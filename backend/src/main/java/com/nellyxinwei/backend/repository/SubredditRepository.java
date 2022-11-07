0c35cfb5bad498ff3aea93825b117da37983691a
根島憲紀-魏新栩 <nelsonalbajr@yahoo.com>
2022-11-07 16:48:58 +0900

    2754 @e bug

--- backend/src/main/java/com/nellyxinwei/backend/repository/SubredditRepository.java
+++ backend/src/main/java/com/nellyxinwei/backend/repository/SubredditRepository.java
@@ -1,4 +1,4 @@
-package com.nellyxinwei.backend.model;
+package com.nellyxinwei.backend.repository;
 
 import com.nellyxinwei.backend.model.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
